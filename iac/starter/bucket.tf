61842cc75c554806718ecd44bdf693d95d8964d7
WeRockStar <kotchaphan.m@gmail.com>
2024-06-10 21:01:48 +0700

    provision tf backend bucket

---
+++ iac/starter/bucket.tf
@@ -0,0 +1,16 @@
+resource "google_storage_bucket" "tf-bucket" {
+  name                     = "data-pipeline-tfstate-425016"
+  location                 = var.region
+  storage_class            = "STANDARD"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+
+  versioning {
+    enabled = true
+  }
+
+  uniform_bucket_level_access = true
+  soft_delete_policy {
+    retention_duration_seconds = 604800 // 7 days
+  }
+}
--- iac/starter/outputs.tf
+++ iac/starter/outputs.tf
@@ -1,7 +1,3 @@
-output "vpc" {
-  value = google_compute_network.vpc_network.self_link
-}
-
-output "subnet" {
-  value = google_compute_subnetwork.vpc_subnetwork.self_link
+output "backend_bucket" {
+  value = google_storage_bucket.tf-bucket.name
 }
