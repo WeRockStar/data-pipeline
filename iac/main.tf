fa6cf3b2441a98bfc2899a3465da54eb61ebb089
WeRockStar <kotchaphan.m@gmail.com>
2024-08-10 11:28:44 +0700

    remove unused Terraform code

--- iac/main.tf
+++ iac/main.tf
@@ -7,24 +7,3 @@ module "gke" {
   node_count   = 3
   location     = var.location
 }
-
-
-# module "airbyte" {
-#   source                 = "./airbyte"
-#   cluster_name           = var.cluster_name
-#   location               = var.location
-#   cluster_endpoint       = module.gke.kubernetes_cluster_host
-#   cluster_ca_certificate = module.gke.cluster_ca_certificate
-#   zone_id                = var.zone_id
-#   cloudflare_api_token   = var.cloudflare_api_token
-# }
-
-# module "airflow" {
-#   source                 = "./airflow"
-#   cluster_name           = var.cluster_name
-#   location               = var.location
-#   cluster_endpoint       = module.gke.kubernetes_cluster_host
-#   cluster_ca_certificate = module.gke.cluster_ca_certificate
-#   zone_id                = var.zone_id
-#   cloudflare_api_token   = var.cloudflare_api_token
-# }
